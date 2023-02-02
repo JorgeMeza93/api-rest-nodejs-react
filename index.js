e7c899000826e15510ef9d142c151e49e3dabc30
Jorge Meza Bello <jorberiver@gmail.com>
2023-02-01 20:25:04 -0600

    Habilitacion de cors

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ import router from "./routes/index.js";
 import conectarDB from "./config/db.js";
 import routerProductos from "./routes/productosRoutes.js";
 import routerPedidos from "./routes/pedidosRoutes.js";
+import cors from "cors";
 
 const app = express();
-const port = 3000;
+const port = 3300;
 
 conectarDB();
 app.use(express.json());
 //Habilitar la lectura de datos desde el request
 app.use( express.urlencoded({ extended: true }))
+app.use(cors());
 app.use("/", router);
 app.use("/", routerProductos);
 app.use("/", routerPedidos)
