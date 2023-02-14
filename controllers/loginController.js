ad3c33ddb746d004da89f7e77684f1da7af6a12a
Jorge Meza Bello <jorberiver@gmail.com>
2023-02-13 23:16:39 -0600

    endpoint para registrar usuario nuevo

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,19 @@
+import Usuario from "../models/Usuarios.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-const registrarUsuario = () => {
-
+const registrarUsuario = async (req, res, next) => {
+    const { password } = req.body;
+    const usuario = new Usuario(req.body);
+    const salt = await bcrypt.genSalt(10);
+    usuario.password = await bcrypt.hash(password, salt);
+    try {
+        await usuario.save();
+        res.json({ mensaje: "Usuario creado correctamente" })
+    } catch (error) {
+        console.log(error);
+        res.json({ mensaje: "Ha ocurrido un error" });
+    }
 }
 
 const autenticarUsuario = () => {
